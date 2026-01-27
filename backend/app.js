42cecc9881e113218af443c96b3323d9b5cfbc9f
juho lee <jxuholee@gmail.com>
2026-01-27 11:50:17 +0100

    Deploy in Render

--- backend/app.js
+++ backend/app.js
@@ -43,9 +43,9 @@ const swaggerUi = require('swagger-ui-express');
 
 const app = express();
 
+app.set('trust proxy', 1);
 const MONGO_URI = process.env.MONGO_URI;
 const PORT = process.env.PORT || 5000;
-
 
 // API 속도 제한 (동일 IP에서 1시간 동안 1000회 요청으로 제한)
 const limiter = rateLimit({
@@ -56,10 +56,22 @@ const limiter = rateLimit({
 app.use('/api', limiter); // /api로 시작하는 모든 라우트에 적용
 
 
+const allowedOrigins = [
+  'https://chemnitz-cultural-sites.onrender.com',
+  'http://localhost:5173', // Vite 기본 포트
+  'http://localhost:3000'
+];
+
 app.use(express.json());
 app.use(cors({
-  origin: 'http://localhost:3000', // 프론트엔드 URL (나중에 실제 도메인으로 변경)
-  credentials: true // 쿠키 전송 허용
+  origin: function (origin, callback) {
+    if (!origin || allowedOrigins.indexOf(origin) !== -1) {
+      callback(null, true);
+    } else {
+      callback(new Error('Not allowed by CORS'));
+    }
+  },
+  credentials: true
 }));
 app.use(cookieParser())
 app.use(express.static(`${__dirname}/public`));
@@ -83,7 +95,7 @@ app.get('/', (req, res) => {
   res.send('Message from the server: Server is Running!');
 });
 
-app.all('/{*any}', (req, res, next) => {
+app.all('*', (req, res, next) => {
   next(new AppError(`Can't find ${req.originalUrl} on this server!`, 404));
 });
 
@@ -104,20 +116,20 @@ mongoose.connect(MONGO_URI)
       // process.exit(1); // 치명적인 오류로 간주하고 앱 종료
     }
 
-    // cron 스케줄러 실행
-    cron.schedule('0 0 * * 0', async () => {
-      console.log('weekly Overpass data update task started...');
-      try {
-        await overpassUpdater();
-        console.log('weekly Overpass data update task completed successfully.');
-      } catch (error) {
-        console.error('Error during weekly Overpass data update task:', error);
-      }
-    }, {
-      scheduled: true, // 스케줄을 즉시 활성화합니다.
-      timezone: "Europe/Berlin" // Chemnitz는 베를린 시간대이므로 명시적으로 지정합니다. 필요에 따라 변경.
-    });
-    console.log('Overpass data update scheduled for every Sunday 00:00.');
+    // // cron 스케줄러 실행
+    // cron.schedule('0 0 * * 0', async () => {
+    //   console.log('weekly Overpass data update task started...');
+    //   try {
+    //     await overpassUpdater();
+    //     console.log('weekly Overpass data update task completed successfully.');
+    //   } catch (error) {
+    //     console.error('Error during weekly Overpass data update task:', error);
+    //   }
+    // }, {
+    //   scheduled: true, // 스케줄을 즉시 활성화합니다.
+    //   timezone: "Europe/Berlin" // Chemnitz는 베를린 시간대이므로 명시적으로 지정합니다. 필요에 따라 변경.
+    // });
+    // console.log('Overpass data update scheduled for every Sunday 00:00.');
 
     app.listen(PORT, () => {
       console.log(`Server is running on http://localhost:${PORT}`);
