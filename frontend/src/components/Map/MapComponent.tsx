393a97eec6f564ff65eabc1448ae0460180b6681
Juho Lee <jxuholee@gmail.com>
2026-05-22 13:21:27 +0200

    perf: map tile image from png to webp

--- frontend/src/components/Map/MapComponent.tsx
+++ frontend/src/components/Map/MapComponent.tsx
@@ -655,7 +655,7 @@ const MapComponent = () => {
         <CurrentLocationButton maxBounds={mapMaxBounds} />
         <TileLayer
           attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors &copy; <a href="https://carto.com/attributions">CARTO</a>'
-          url="https://{s}.basemaps.cartocdn.com/light_all/{z}/{x}/{y}{r}.png"
+          url="https://{s}.basemaps.cartocdn.com/light_all/{z}/{x}/{y}{r}.webp"
           maxZoom={20}
         />
         <ZoomControl position="bottomright" />
